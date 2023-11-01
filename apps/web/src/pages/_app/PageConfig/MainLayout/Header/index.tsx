0d972513af47c5841f7742b3b9822fd78dc4890b
TERtISmu <hERomaCe@proton.me>
2023-11-01 21:33:04 +0300

    feat(web): add log out functionality

--- apps/web/src/pages/_app/PageConfig/MainLayout/Header/index.tsx
+++ apps/web/src/pages/_app/PageConfig/MainLayout/Header/index.tsx
@@ -8,68 +8,78 @@ import {
 } from '@mantine/core';
 import { CartIcon, LogoutIcon, ShopyIcon } from 'public/icons';
 import Link from 'next/link';
+import { accountApi } from 'resources/account';
 
-const Header: FC = () => (
-  <LayoutHeader height="104px" ff="Inter" withBorder={false}>
-    <Group
-      w={1345}
-      mt={32}
-      mb={32}
-      mx="auto"
-      style={{ justifyContent: 'space-between' }}
-    >
-      <Group spacing={6}>
-        <Avatar>
-          <ShopyIcon />
-        </Avatar>
-        <Title order={2} ff="Inter" style={{ fontSize: '29px' }}>
-          Shopy
-        </Title>
+const Header: FC = () => {
+  const { mutate: signOut } = accountApi.useSignOut();
+  return (
+    <LayoutHeader height="104px" ff="Inter" withBorder={false}>
+      <Group
+        w={1345}
+        mt={32}
+        mb={32}
+        mx="auto"
+        style={{ justifyContent: 'space-between' }}
+      >
+        <Group spacing={6}>
+          <Avatar>
+            <ShopyIcon />
+          </Avatar>
+          <Title order={2} ff="Inter" style={{ fontSize: '29px' }}>
+            Shopy
+          </Title>
+        </Group>
+        <Group spacing={32} mr={16}>
+          <Link
+            href="/"
+            style={{
+              fontFamily: 'Inter',
+              fontSize: '16px',
+              color: '#201F22',
+              fontWeight: '500',
+              lineHeight: '32px',
+              textDecoration: 'none',
+              backgroundColor: '#ECECEE',
+              borderRadius: '20px',
+              padding: '2px 20px',
+            }}
+          >
+            Marketplace
+          </Link>
+          <Link
+            href="/your-products"
+            style={{
+              fontFamily: 'Inter',
+              fontSize: '16px',
+              color: '#201F22',
+              fontWeight: '500',
+              lineHeight: '32px',
+              textDecoration: 'none',
+              backgroundColor: '#ECECEE',
+              borderRadius: '20px',
+              padding: '2px 20px',
+            }}
+          >
+            Your Products
+          </Link>
+        </Group>
+        <Group spacing={32}>
+          <ActionIcon color="gray" variant="transparent" w={40} h={40}>
+            <CartIcon />
+          </ActionIcon>
+          <ActionIcon
+            color="gray"
+            variant="transparent"
+            w={40}
+            h={40}
+            onClick={() => signOut()}
+          >
+            <LogoutIcon />
+          </ActionIcon>
+        </Group>
       </Group>
-      <Group spacing={32} mr={16}>
-        <Link
-          href="/"
-          style={{
-            fontFamily: 'Inter',
-            fontSize: '16px',
-            color: '#201F22',
-            fontWeight: '500',
-            lineHeight: '32px',
-            textDecoration: 'none',
-            backgroundColor: '#ECECEE',
-            borderRadius: '20px',
-            padding: '2px 20px',
-          }}
-        >
-          Marketplace
-        </Link>
-        <Link
-          href="/your-products"
-          style={{
-            fontFamily: 'Inter',
-            fontSize: '16px',
-            color: '#201F22',
-            fontWeight: '500',
-            lineHeight: '32px',
-            textDecoration: 'none',
-            backgroundColor: '#ECECEE',
-            borderRadius: '20px',
-            padding: '2px 20px',
-          }}
-        >
-          Your Products
-        </Link>
-      </Group>
-      <Group spacing={32}>
-        <ActionIcon color="gray" variant="transparent" w={40} h={40}>
-          <CartIcon />
-        </ActionIcon>
-        <ActionIcon color="gray" variant="transparent" w={40} h={40}>
-          <LogoutIcon />
-        </ActionIcon>
-      </Group>
-    </Group>
-  </LayoutHeader>
-);
+    </LayoutHeader>
+  );
+};
 
 export default memo(Header);
